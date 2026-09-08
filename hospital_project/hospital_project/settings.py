177d02a6678e7c24e1931ba670f024940118c89d
belindairakoze37 <belindairakoze37@gmail.com>
2026-09-08 15:19:37 +0300

    Modify settings

--- hospital_project/hospital_project/settings.py
+++ hospital_project/hospital_project/settings.py
@@ -27,15 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Email configuration (you had MAILERS but should be EMAIL)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# For production:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your-email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'your-password'
+
 
 # JWT Settings
 from datetime import timedelta
